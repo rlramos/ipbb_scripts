788420e11dac94db184d878f9b334465e9276e41
rlramos <45953562+rlramos@users.noreply.github.com>
2023-12-18 10:34:24 -0800

    Create move.py

---
+++ move.py
@@ -0,0 +1,42 @@
+#!/bin/env/python3.8
+
+import pyautogui as pg
+import time
+
+def move():
+    print('Initiating...')
+    pg.moveTo(1000, 0, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 9 mins')
+    pg.moveTo(1000, 1000, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 18 mins')
+    pg.moveTo(1000, 0, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 36 mins')
+    pg.moveTo(1000, 1000, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 45 mins')
+    pg.moveTo(1000, 0, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 54 mins')
+    pg.moveTo(1000, 1000, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 63 mins')
+    pg.moveTo(1000, 0, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 72 mins')
+    pg.moveTo(1000, 1000, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 81 mins')
+    pg.moveTo(1000, 0, duration=1)
+    time.sleep(540)
+    print('Time elapsed: 90 mins')
+    pg.moveTo(1000, 1000, duration=1)
+    print('SEQUENCE ENDED')
+    
+    
+
+    
+move()
+
